4abe46c42ee456941dd1a2ec3b87718d05876319
LiamFlahive <williamflahive2012@u.northwestern.edu>
2013-08-28 12:20:43 -0400

    messages displaying

--- public/js/message-dispaly.js
+++ public/js/message-dispaly.js
@@ -1,51 +1,59 @@
 
 'use strict';
 
-var user = getCookie('user');
+var req = new XMLHttpRequest()
+// Open a GET request to "/all_dbs"
+req.open("GET", "/db/messages/MA112JL7")
+// Send nothing as the request body
+req.send("")
+// Get the response
+console.log(req.responseText);
 
-var couch = sag.server('localhost', 5000);
+// var user = getCookie('user');
 
-couch.setPathPrefix('/db');
+// var couch = sag.server('./');
+
+// couch.setPathPrefix('/db');
  
-    // Set the database
-    couch.setDatabase('messages');
+//     // Set the database
+//     couch.setDatabase('messages');
 
-    couch.login({
-      user: 'liamflahive',
-      pass: 'swatter5',
-      type: couch.AUTH_BASIC
-    });
+//     couch.login({
+//       user: 'liamflahive',
+//       pass: 'swatter5',
+//       type: couch.AUTH_BASIC
+//     });
 
-    couch.get({
-      url: '/'+user,
-      callback: function(resp, success) {
-      	var doc;
-      	if(success) {
-          var doc = resp.body;
-          var inMessages = doc.recievedMssgLog;
-          var outMessages = doc.sentMssgLog;
-          for(var i = 0; i < inMessages.length; i++){
-				(function(foo){
-				$(document).ready(function(){
-					    $('#messagesSection').append("<div class='small-12 columns small-centered receivedWrapper'><div class='messageBox small-12 columns'><div class='row messageHead'><div class='from columns small-4'><p>From: <span class='fromName' id='from12345'>Joe Smith</span></p></div><div class='date columns right small-4'><p>Sent: <span class='dateRec' id='date12345'>1/1/2001</span></p></div></div><div class='row contentWrapper'><div class='messageContent columns'><p><span class='contentRec' id='contentRec12345'>"+inMessages[foo]+"</span></p></div></div><ul class='messageFunctions columns'><li class='like'><a href='#likepost12345'>Like <i class='foundicon-up-arrow functionImage'></i></a></li><li class='dislike'><a href='#dislikepost12345'>Dislike <i class='foundicon-down-arrow functionImage'></i></a></li><li class='reply'><a href='#replyPost12345'>Reply <i class='foundicon-add-doc functionImage'></i></a></li><li class='report'><a href='#reportPost12345'>Report <i class='foundicon-flag functionImage'></i></a></li><li class='archive'><a href='#archivePost12345'>Archive <i class='foundicon-trash functionImage'></i></a></li></ul></div></div> ");
+//     couch.get({
+//       url: '/'+user,
+//       callback: function(resp, success) {
+//       	var doc;
+//       	if(success) {
+//           var doc = resp.body;
+//           var inMessages = doc.recievedMssgLog;
+//           var outMessages = doc.sentMssgLog;
+//           for(var i = 0; i < inMessages.length; i++){
+// 				(function(foo){
+// 				$(document).ready(function(){
+// 					    $('#messagesSection').append("<div class='small-12 columns small-centered receivedWrapper'><div class='messageBox small-12 columns'><div class='row messageHead'><div class='from columns small-4'><p>From: <span class='fromName' id='from12345'>Joe Smith</span></p></div><div class='date columns right small-4'><p>Sent: <span class='dateRec' id='date12345'>1/1/2001</span></p></div></div><div class='row contentWrapper'><div class='messageContent columns'><p><span class='contentRec' id='contentRec12345'>"+inMessages[foo]+"</span></p></div></div><ul class='messageFunctions columns'><li class='like'><a href='#likepost12345'>Like <i class='foundicon-up-arrow functionImage'></i></a></li><li class='dislike'><a href='#dislikepost12345'>Dislike <i class='foundicon-down-arrow functionImage'></i></a></li><li class='reply'><a href='#replyPost12345'>Reply <i class='foundicon-add-doc functionImage'></i></a></li><li class='report'><a href='#reportPost12345'>Report <i class='foundicon-flag functionImage'></i></a></li><li class='archive'><a href='#archivePost12345'>Archive <i class='foundicon-trash functionImage'></i></a></li></ul></div></div> ");
 
 
-					});
-				}(i));
-			};
-		   for(var i = 0; i < outMessages.length; i++){
-				(function(foo){
-				$(document).ready(function(){
-					    $('#outbox').append("<div class='small-12 columns small-centered receivedWrapper'><div class='messageBox small-12 columns'><div class='row messageHead'><div class='from columns small-4'><p>From: <span class='fromName' id='from12345'>Joe Smith</span></p></div><div class='date columns right small-4'><p>Sent: <span class='dateRec' id='date12345'>1/1/2001</span></p></div></div><div class='row contentWrapper'><div class='messageContent columns'><p><span class='contentRec' id='contentRec12345'>"+outMessages[foo]+"</span></p></div></div><ul class='messageFunctions columns'><li class='like'><a href='#likepost12345'>Like <i class='foundicon-up-arrow functionImage'></i></a></li><li class='dislike'><a href='#dislikepost12345'>Dislike <i class='foundicon-down-arrow functionImage'></i></a></li><li class='reply'><a href='#replyPost12345'>Reply <i class='foundicon-add-doc functionImage'></i></a></li><li class='report'><a href='#reportPost12345'>Report <i class='foundicon-flag functionImage'></i></a></li><li class='archive'><a href='#archivePost12345'>Archive <i class='foundicon-trash functionImage'></i></a></li></ul></div></div> ");
-					});
-				}(i));
-			};
-        }
-        else if(resp._HTTP.status == 404) {
-          alert('boo');
-          };
-      }
-  	});
+// 					});
+// 				}(i));
+// 			};
+// 		   for(var i = 0; i < outMessages.length; i++){
+// 				(function(foo){
+// 				$(document).ready(function(){
+// 					    $('#outbox').append("<div class='small-12 columns small-centered receivedWrapper'><div class='messageBox small-12 columns'><div class='row messageHead'><div class='from columns small-4'><p>From: <span class='fromName' id='from12345'>Joe Smith</span></p></div><div class='date columns right small-4'><p>Sent: <span class='dateRec' id='date12345'>1/1/2001</span></p></div></div><div class='row contentWrapper'><div class='messageContent columns'><p><span class='contentRec' id='contentRec12345'>"+outMessages[foo]+"</span></p></div></div><ul class='messageFunctions columns'><li class='like'><a href='#likepost12345'>Like <i class='foundicon-up-arrow functionImage'></i></a></li><li class='dislike'><a href='#dislikepost12345'>Dislike <i class='foundicon-down-arrow functionImage'></i></a></li><li class='reply'><a href='#replyPost12345'>Reply <i class='foundicon-add-doc functionImage'></i></a></li><li class='report'><a href='#reportPost12345'>Report <i class='foundicon-flag functionImage'></i></a></li><li class='archive'><a href='#archivePost12345'>Archive <i class='foundicon-trash functionImage'></i></a></li></ul></div></div> ");
+// 					});
+// 				}(i));
+// 			};
+//         }
+//         else if(resp._HTTP.status == 404) {
+//           alert('boo');
+//           };
+//       }
+//   	});
 
 
 
